ba77d1576682063310b86d8ac9e666ac36fc9d2d
FANGFANG1757 <grace.gao13@gmail.com>
2024-03-27 22:30:00 +0800

    Add initial structure for Todo List App

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <div>
+      <h1>To Do List</h1>
+      <form className="todo-form">
+        <input
+          className="todo-input"
+          type="text"
+          placeholder="Add a new todo"
+        />
+        <button className="input-btn">Add</button>
+      </form>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <ul className="todo-list">
+          <li className="list-item">
+            <div className="todo-item">
+              <input type="checkbox" />
+              Todo Item 1
+            </div>
+            <button className="edit-btn">Edit</button>
+            <button className="delete-btn">Delete</button>
+          </li>
+          <li className="list-item">
+            <div className="todo-item">
+              <input type="checkbox" />
+              Todo Item 2
+            </div>
+            <button className="edit-btn">Edit</button>
+            <button className="delete-btn">Delete</button>
+          </li>
+          <li className="list-item">
+            <div className="todo-item">
+              <input type="checkbox" />
+              Todo Item 3
+            </div>
+            <button className="edit-btn">Edit</button>
+            <button className="delete-btn">Delete</button>
+          </li>
+          <li className="list-item">
+            <div className="todo-item">
+              <input type="checkbox" />
+              Todo Item 4
+            </div>
+            <button className="edit-btn">Edit</button>
+            <button className="delete-btn">Delete</button>
+          </li>
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
